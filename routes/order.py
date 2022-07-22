5012b84b619f19c7220d77abb620e51368de50f3
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-22 10:46:12 -0500

    routes-updated

--- routes/order.py
+++ routes/order.py
@@ -1,8 +1,7 @@
-from tokenize import Double
 from fastapi.routing import APIRouter
 from fastapi.responses import JSONResponse
 from sqlalchemy import select
-from models.buy import tableOrder, tableOrderDetails,tableSaucer,tableShop
+from models.buy import tableOrder, tableOrderDetails,tableSaucer
 from schemas.buy import OrderSchema,OrderDetailSchema
 from config.db import conn
 
@@ -10,7 +9,7 @@ buyRoute = APIRouter()
 
 @buyRoute.get("/")
 def get_root():
-    JSONResponse({"Ok": ":)"}, status_code=200)
+    return {"message": ":)"}
 
 @buyRoute.post("/user/buy")
 def buy_product(order: OrderSchema):
@@ -23,21 +22,31 @@ def buy_product(order: OrderSchema):
     except Exception as e:
         return JSONResponse({"Error": str(e)}, status_code=500)
 
+
+
 @buyRoute.get("/user/orders")
 def get_orders(user_id: int):
     try:
-        orders = conn.execute(select([tableOrder.c.id,tableOrderDetails.c.saucer_id,tableOrder.c.user_id,tableSaucer.c.name,tableSaucer.c.price,tableOrder.c.date_purchase,tableOrder.c.total]).select_from(tableOrder.join(tableOrderDetails,tableOrder.c.id == tableOrderDetails.c.order_id).join(tableSaucer,tableSaucer.c.id == tableOrderDetails.c.saucer_id)).where(tableOrder.c.user_id == user_id)).fetchall()
-        list_orders = []
+        orders = conn.execute(select([tableOrder.c.id, tableOrder.c.total, tableOrder.c.date_purchase]).where(tableOrder.c.user_id == user_id)).fetchall()
+        order_list = []
+        for order in orders:
+            order_list.append({"orden": order[0], "total": str(order[1]), "date_purchase": str(order[2])})
+        return JSONResponse({"orders": order_list}, status_code=200)
+    except Exception as e:
+        return JSONResponse({"Error": str(e)}, status_code=500)
+
+@buyRoute.get("/user/orders-details")
+def get_orders_detail(order_id: int):
+    try:
+        orders = conn.execute(select([tableOrder.c.id,tableSaucer.c.name,tableSaucer.c.price]).select_from(tableOrder.join(tableOrderDetails,tableOrder.c.id == tableOrderDetails.c.order_id).join(tableSaucer,tableSaucer.c.id == tableOrderDetails.c.saucer_id)).where(tableOrder.c.id == order_id)).fetchall()
+        list_orders = [] 
+
         for order in orders:
             list_orders.append({
-                "order_id": order[0],
-                "saucer_id": order[1],
-                "user_id": order[2],
-                "saucer": order[3],
-                "price": str(order[4]),
-                "date_purchase": str(order[5]),
-                "total": str(order[6])
+                "platillo": order[1],
+                "precio": str(order[2]),
             })
-        return JSONResponse({"orders":list_orders}, status_code=200)
+
+        return JSONResponse({"order_details":list_orders})
     except Exception as e:
         return JSONResponse({"Error": str(e)}, status_code=500)
