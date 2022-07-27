b79c5bb737872ae878ca6dbcf13b13421ef90c5c
Jhonathan Miceli Landa <jhonathanmicelilanda@MacBook-Air-de-Jhonathan.local>
2022-07-26 22:25:56 -0500

    sessions

--- config/db.py
+++ config/db.py
@@ -1,4 +1,8 @@
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+
 engine = create_engine(url="mysql+pymysql://ufood:ufood2022@ufoodb.c2ppwslmpouq.us-east-1.rds.amazonaws.com:3306/ufood", echo=False)
 meta = MetaData()
 conn = engine.connect()
+Session = sessionmaker(bind=engine,autocommit=True)
+session = Session()
--- routes/order.py
+++ routes/order.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy import select
 from models.buy import tableOrder, tableOrderDetails,tableSaucer
 from schemas.buy import OrderSchema,OrderDetailSchema
-from config.db import conn
+from config.db import conn,session
 
 buyRoute = APIRouter()
 
@@ -14,10 +14,10 @@ async def get_root():
 @buyRoute.post("/user/buy")
 async def buy_product(order: OrderSchema):
     try:
-        conn.execute(tableOrder.insert().values(total=float(order.total), date_purchase=order.date_purchase, user_id=order.user_id))
-        order_id = conn.execute(tableOrder.select().order_by(tableOrder.c.id.desc()).limit(1)).first()[0]
+        session.execute(tableOrder.insert().values(total=float(order.total), date_purchase=order.date_purchase, user_id=order.user_id))
+        order_id = session.execute(tableOrder.select().order_by(tableOrder.c.id.desc()).limit(1)).first()[0]
         for product in order.products.split(","):
-            conn.execute(tableOrderDetails.insert().values(order_id=order_id, saucer_id=int(product)))
+            session.execute(tableOrderDetails.insert().values(order_id=order_id, saucer_id=int(product)))
         return JSONResponse({"message": "Orden creada correctamente"}, status_code=200)
     except Exception as e:
         return JSONResponse({"Error": str(e)}, status_code=500)
@@ -27,7 +27,7 @@ async def buy_product(order: OrderSchema):
 @buyRoute.get("/user/orders")
 async def get_orders(user_id: int):
     try:
-        orders = conn.execute(select([tableOrder.c.id, tableOrder.c.total, tableOrder.c.date_purchase]).where(tableOrder.c.user_id == user_id)).fetchall()
+        orders = session.execute(select([tableOrder.c.id, tableOrder.c.total, tableOrder.c.date_purchase]).where(tableOrder.c.user_id == user_id)).fetchall()
         order_list = []
         for order in orders:
             order_list.append({"orden": order[0], "total": str(order[1]), "date_purchase": str(order[2])})
@@ -38,7 +38,7 @@ async def get_orders(user_id: int):
 @buyRoute.get("/user/orders-details")
 async def get_orders_detail(order_id: int):
     try:
-        orders = conn.execute(select([tableOrder.c.id,tableSaucer.c.name,tableSaucer.c.price]).select_from(tableOrder.join(tableOrderDetails,tableOrder.c.id == tableOrderDetails.c.order_id).join(tableSaucer,tableSaucer.c.id == tableOrderDetails.c.saucer_id)).where(tableOrder.c.id == order_id)).fetchall()
+        orders = session.execute(select([tableOrder.c.id,tableSaucer.c.name,tableSaucer.c.price]).select_from(tableOrder.join(tableOrderDetails,tableOrder.c.id == tableOrderDetails.c.order_id).join(tableSaucer,tableSaucer.c.id == tableOrderDetails.c.saucer_id)).where(tableOrder.c.id == order_id)).fetchall()
         list_orders = [] 
 
         for order in orders:
